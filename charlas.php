06294de767b68658ea0a77349b47cb1dcc313d38
Nisa Delgado <nisadelgado@gmail.com>
2021-07-14 00:00:43 -0400

    Charla en DevTeam504 agregada

--- charlas.php
+++ charlas.php
@@ -83,6 +83,17 @@
 
                 </div>
             </div>
+
+            <div class="col-lg-10 col-md-10 mx-auto">
+                <div class="text-center m-4">
+                    <h4>DevTeam504</h4>
+
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/S1SssS-rE8Y?AutoHide=1&showinfo=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                        
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 
