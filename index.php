9637ea933e8e8c16d6d7f2d68ee54dc5922c7af8
Nisa Delgado <nisadelgado@gmail.com>
2021-09-11 14:50:38 -0400

    Icono agregado

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="icon" type="image/png" href="img/favicon.png">
+
 <title>Nisa Delgado</title>
 
 <link href="node_modules/tailwindcss/dist/tailwind.css" rel="stylesheet">
